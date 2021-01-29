b21903202e963fc0d991e953ed528574d1589784
A501Xtreme <alexander.romero-jones@patelhs.org>
2021-01-29 15:32:28 +0000

    started proj 4

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("How much do your retros cost?");
+    int price = scan.nextInt();
+    System.out.println("How much did your airmaxes cost? ");
+    int priceTwo = scan.nextInt();
+    System.out.println("How much did your air forces cost ");
+    int priceThree = scan.nextInt();
+  }
+
+  static int shoeComp(int price, int priceTwo, int priceThree)
+  {
+   int priceComp = price - priceTwo;
   }
 }
